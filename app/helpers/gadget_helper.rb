15b69b14c226d3c47454266c95c6a22a8eb964c5
Max Prokopiev <me@maxprokopiev.com>
2014-06-14 17:15:35 +0400

    Add switch for modes

--- app/helpers/gadget_helper.rb
+++ app/helpers/gadget_helper.rb
@@ -2,4 +2,14 @@ module GadgetHelper
   def cover_picture_for(gadget)
     image_tag gadget.pictures.first.image.thumb if gadget.pictures.any?
   end
+
+  def classes_for_modes(mode, current_mode)
+    classes = 'btn btn-default'
+    classes << ' active' if current_mode == mode
+    classes
+  end
+
+  def link_to_mode(mode)
+    link_to mode.humanize, gadgets_path(mode: mode), class: classes_for_modes(mode, params[:mode])
+  end
 end
